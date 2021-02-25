3fe508be90cec0acb011c838bab66b0a647bad46
Johannes Rebling <johannesrebling@gmail.com>
2021-02-25 11:47:17 +0100

    init asignment 1

--- assessments/m1/README.md
+++ assessments/m1/README.md
@@ -1,1 +1,3 @@
 /* Add Author and Project Details here */
+Author: Johannes Rebling 
+First assignemnt for Coursera course...
--- assessments/m1/stats.c
+++ assessments/m1/stats.c
@@ -9,13 +9,13 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file stats.c
+ * @brief Get statstics I guess...
  *
- * <Add Extended Description Here>
+ * Calculates means and maxes and other stuffses
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author Johannes Rebling
+ * @date 02/2021
  *
  */
 
@@ -28,16 +28,40 @@
 #define SIZE (40)
 
 void main() {
-
-  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
-                              114, 88,   45,  76, 123,  87,  25,  23,
-                              200, 122, 150, 90,   92,  87, 177, 244,
-                              201,   6,  12,  60,   8,   2,   5,  67,
-                                7,  87, 250, 230,  99,   3, 100,  90};
+  unsigned char test[SIZE] = {34, 201, 190, 154, 8, 194, 2, 6,
+                                     114, 88, 45, 76, 123, 87, 25, 23,
+                                     200, 122, 150, 90, 92, 87, 177, 244,
+                                     201, 6, 12, 60, 8, 2, 5, 67,
+                                     7, 87, 250, 230, 99, 3, 100, 90};
 
   /* Other Variable Declarations Go Here */
   /* Statistics and Printing Functions Go Here */
 
+  printf("Hello workd!\n");
 }
 
 /* Add other Implementation File Code Here */
+
+// print_statistics()
+// A function that prints the statistics of an array including minimum,
+//  maximum, mean, and median.
+
+// print_array()  
+// Given an array of data and a length, prints the array to the screen
+
+// find_median()
+// Given an array of data and a length, returns the median value
+
+// find_mean()
+// Given an array of data and a length, returns the mean
+
+// find_maximum()
+// Given an array of data and a length, returns the maximum
+
+// find_minimum()
+// Given an array of data and a length, returns the minimum
+
+// sort_array()
+// Given an array of data and a length, sorts the array from largest to smallest. 
+//  (The zeroth Element should be the largest value, and the last element (n-1) 
+//  should be the smallest value. )
